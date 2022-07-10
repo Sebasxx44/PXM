74ee049f910d4514a259ac915f1841926f748ec6
Sebastian sanchez <sanchezz3s47@gmail.com>
2022-07-10 08:35:50 -0500

    nuevos

--- script.js
+++ script.js
@@ -83,23 +83,24 @@ window.addEventListener('load', function(){
         },
 
         responsive: [
+            // {
+            //   // screens greater than >= 775px
+            //   breakpoint: 600,
+            //   settings: {
+            //     // Set to `auto` and provide item width to adjust to viewport
+            //     slidesToShow: '3',
+            //     slidesToScroll: '1',
+            //     itemWidth: 150,
+            //     duration: 0.25
+            //   }
+            // },
             {
-              // screens greater than >= 775px
-              breakpoint: 600,
-              settings: {
-                // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: '3',
-                slidesToScroll: '1',
-                itemWidth: 150,
-                duration: 0.25
-              }
-            },{
               // screens greater than >= 1024px
               breakpoint: 1024,
               settings: {
-                slidesToShow: 6,
-                slidesToScroll: 2,
-                itemWidth: 150,
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                itemWidth: 100,
                 duration: 0.25
               }
             },{
@@ -107,7 +108,7 @@ window.addEventListener('load', function(){
               breakpoint: 200,
               settings: {
                 // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: '2',
+                slidesToShow: 4,
                 slidesToScroll: '1',
                 itemWidth: 60,
                 duration: 0.25
